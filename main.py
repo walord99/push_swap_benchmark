4090a528abae6168caa96903fd2cd423163c9416
Walord <benplante99@gmail.com>
2023-12-25 23:34:30 -0500

    multiprocessing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import subprocess
 import sys
+from multiprocessing import Process, Value, Array
+
+path = ''
 
 def main():
 
@@ -8,25 +11,35 @@ def main():
         print("arguments: path, iteration")
         sys.exit(1)
     try:
-        path = sys.argv[1]
+        globals()['path'] = sys.argv[1]
         iteration = int(sys.argv[2])
-        print("%s" %(run_test(10, iteration, path)))
-        print("%s" %(run_test(100, iteration, path)))
-        print("%s" %(run_test(500, iteration, path)))
-    except:
-        print("invalide args")
+        print("%s" %(run_test(10, iteration)))
+        print("%s" %(run_test(100, iteration)))
+        print("%s" %(run_test(500, iteration)))
+    except Exception as error:
+        print("invalide args", error)
 
-def run_test(list_size, iteration, path):
+def child(return_array, pos, list_size):
+    list = ""
+    rand = list_generator(list_size)
+    for num in rand:
+        list += f"%s " %(str(num))
+    return_array[pos] = int(subprocess.getoutput(f'%s %s | wc -l' %(path, str(list))))
+
+def run_test(list_size, iteration):
     total = 0
 
-    i = iteration;
-    while i != 0:
-        list = ""
-        rand = list_generator(list_size)
-        for num in rand:
-            list += f"%s " %(str(num))
-        total += int(subprocess.getoutput(f'%s %s | wc -l' %(path, str(list))))
-        i -= 1
+    op_count = Array('i' ,iteration)
+    i = 0
+    p = []
+    while i != iteration:
+        p.append(Process(target=child, args=(op_count, i, list_size)))
+        p[i].start()
+        i += 1
+    for proc in p:
+        proc.join()
+    for i in op_count:
+        total += i
     return total / iteration
 
 def list_generator(size):
